e30be360c906421b867bc0ffa1ded3c0375e29b1
Henefisa <cherishnghia@gmail.com>
2020-12-26 23:32:16 +0700

    exam

--- src/main/java/API/IUser.java
+++ src/main/java/API/IUser.java
@@ -8,5 +8,4 @@ import java.rmi.RemoteException;
 public interface IUser extends Remote {
     public boolean register(User user) throws RemoteException;
     public User login(String username, String password) throws RemoteException;
-    public boolean get(String id) throws RemoteException;
 }
---
+++ src/main/java/DataAccessor/QuestionsDA.java
@@ -0,0 +1,72 @@
+package DataAccessor;
+
+import Models.Questions;
+import Utils.ConnectDB;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class QuestionsDA {
+    public boolean createQuestion(Questions questions) {
+        String sql = "insert into Questions values(?, ?, ?, ?, ?, ?, ?, ?)";
+        boolean rowInserted = false;
+        try {
+            ConnectDB.connect();
+            UUID uuid = UUID.randomUUID();
+            PreparedStatement statement = ConnectDB.getConnection().prepareStatement(sql);
+            statement.setString(1, uuid.toString());
+            statement.setString(2, questions.getQuestion());
+            statement.setString(3, questions.getAnswer1());
+            statement.setString(4, questions.getAnswer2());
+            statement.setString(5, questions.getAnswer3());
+            statement.setString(6, questions.getAnswer4());
+            statement.setString(7, questions.getAnswer_true());
+            statement.setString(8, questions.getExam_id());
+            rowInserted = statement.executeUpdate() > 0;
+            statement.close();
+            ConnectDB.disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rowInserted;
+    }
+
+    public List<Questions> getQuestions() {
+        List<Questions> questions = new ArrayList<>();
+        String sql = "select * from Exam";
+
+        try {
+            ConnectDB.connect();
+            Statement statement = ConnectDB.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String question = resultSet.getString("question");
+                String answer1 = resultSet.getString("answer1");
+                String answer2 = resultSet.getString("answer2");
+                String answer3 = resultSet.getString("answer3");
+                String answer4 = resultSet.getString("answer4");
+                String answer_true = resultSet.getString("answer_true");
+                String exam_id = resultSet.getString("exam_id");
+
+
+                Questions questions_item = new Questions(id, question, answer1, answer2, answer3, answer4, answer_true, exam_id);
+                questions.add(questions_item);
+            }
+
+            resultSet.close();
+            statement.close();
+
+            ConnectDB.disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return questions;
+    }
+}
