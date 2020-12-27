96b7a76ac99675a62df0ad600b6e2a26e2f63685
Henefisa <cherishnghia@gmail.com>
2020-12-27 19:14:47 +0700

    leave chat

--- src/main/java/Controllers/TestController.java
+++ src/main/java/Controllers/TestController.java
@@ -1,4 +1,21 @@
 package Controllers;
 
+import API.IExamServer;
+import javafx.fxml.FXML;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class TestController {
+    @FXML
+    private void leaveTest() {
+        try {
+            IExamServer iExamServer = (IExamServer) Naming.lookup("rmi://localhost:9090/examServer");
+            iExamServer.unregister();
+        } catch (NotBoundException | MalformedURLException | RemoteException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
